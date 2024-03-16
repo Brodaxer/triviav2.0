825e829f14e3cde752e1b99856947151b28b232d
Brodaxer <mloszewski123@gmail.com>
2024-03-16 10:51:54 +0100

    a

--- apiquizz/ui.py
+++ apiquizz/ui.py
@@ -48,23 +48,27 @@ class QuizInterface:
 
 
     def get_next_question(self):
-        que_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=que_text)
-        # if self.quiz.still_has_questions():
-        #     self.get_next_question()
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label["text"] = f"Score: {self.quiz.score}"
+            que_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=que_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="End of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
 
     def true_answer(self):
-        self.quiz.check_answer("True")
-        self.score_label["text"] = f"Score: {self.quiz.score}"
-        if self.quiz.still_has_questions():
-            self.get_next_question()
-        else:
-            self.window.quit()
+        self.give_feedback(self.quiz.check_answer("True"))
+
 
     def false_answer(self):
-        self.quiz.check_answer("False")
-        self.score_label["text"] = f"Score: {self.quiz.score}"
-        if self.quiz.still_has_questions():
-            self.get_next_question()
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
         else:
-            self.window.quit()
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
